4cbedd7b02da3fd521cf1c9972738a262c9702f8
Chris <chrisiivix@gmail.com>
2018-08-18 11:38:41 +1000

    Initial commit.

---
+++ src/main/java/com/orange/lightning/Extract.java
@@ -0,0 +1,7 @@
+package com.orange.lightning;
+
+public class Extract {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/orange/lightning/spells/Spell.java
@@ -0,0 +1,4 @@
+package com.orange.lightning.spells;
+
+public class Spell {
+}
